7d6516d0395b27c861fcc35eb336d559ebbcfeaf
Johan Degrieck <johneke@hotmail.com>
2014-06-26 20:11:37 +0200

    Update app.js
    restored to original setup using app.configure and path

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Import the Express module
 var express = require('express');
 
+// Import the 'path' module (packaged with Node.js)
+var path = require('path');
 
 // Create a new instance of Express
 var app = express();
@@ -8,19 +10,20 @@ var app = express();
 // Import the Anagrammatix game file.
 var agx = require('./agxgame');
 
-// Create a simple Express application
+
 /*SPECIFIC CONFIG FOR OPENSHIFT*/
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);  
-app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); 
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 // Turn down the logging activity
 app.use(express.logger('dev'));
 // Serve static html, js, css, and image files from the 'public' directory
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname,'public')));
 
-// Create a Node.js based http server on port 3000
+// Create a Node.js based http server on openSHIFT
 var server = require('http').createServer(app).listen(app.get('port'), app.get('ipaddr'), function(){
-		console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
-	});
+	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
+});
+/*END OPENSHIFT SPECIFIC CODE */
 
 // Create a Socket.IO server and attach it to the http server
 var io = require('socket.io').listen(server);
@@ -33,5 +36,3 @@ io.sockets.on('connection', function (socket) {
     //console.log('client connected');
     agx.initGame(io, socket);
 });
-
-
