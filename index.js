06b3546ead7d7a2b2b358e52d3232f1af3d4dc9f
Johan Degrieck <johneke@hotmail.com>
2014-06-26 17:55:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,19 +11,13 @@ var app = express();
 var agx = require('./agxgame');
 
 // Create a simple Express application
-app.configure(function() {
-
-	/*SPECIFIC CONFIG FOR OPENSHIFT*/
-	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);  
-	app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); 
-	
-    // Turn down the logging activity
-    app.use(express.logger('dev'));
-
-    // Serve static html, js, css, and image files from the 'public' directory
-    app.use(express.static(path.join(__dirname,'public')));
-
-});
+/*SPECIFIC CONFIG FOR OPENSHIFT*/
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);  
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); 
+// Turn down the logging activity
+app.use(express.logger('dev'));
+// Serve static html, js, css, and image files from the 'public' directory
+app.use(express.static(path.join(__dirname,'public')));
 
 // Create a Node.js based http server on port 3000
 var server = require('http').createServer(app).listen(app.get('port'), app.get('ipaddr'), function(){
